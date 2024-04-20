17d0d677f9ac48d4f0d77c6bce68faf85f816ac6
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-04-19 22:56:49 -0400

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -17,7 +19,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return response
 
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
+  // console.log("email: ", email);
   // validation
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -26,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // checking if user already exist or not
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -37,10 +39,14 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for images , avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
 
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path
+  }
+  
 
   if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
+      throw new ApiError(400, "Avatar file is required")
   }
 
   // upload them to cloudinary
@@ -56,11 +62,10 @@ const registerUser = asyncHandler(async (req, res) => {
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
-    email,
+    email, 
     password,
-    username: username.toLowerCase(),
-  });
-
+    username: username.toLowerCase()
+})
   // _id is by default created by mongodb
   // so we will confirm that our user is created or not
   // remove password and refresh token
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,20 +10,22 @@ cloudinary.config({
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
-    if (!localFilePath) return null;
-    // upload the file on cloudinary
-    const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
-    // file has been upload successfully
-    console.log("file is uploaded on cloudinary: ", response.url);
-    return response;
+      if (!localFilePath) return null
+      //upload the file on cloudinary
+      const response = await cloudinary.uploader.upload(localFilePath, {
+          resource_type: "auto"
+      })
+      // file has been uploaded successfull
+      //console.log("file is uploaded on cloudinary ", response.url);
+      fs.unlinkSync(localFilePath)
+      return response;
+
   } catch (error) {
-    // remove the locally saved temporary file as the upload opeartion got failed
-    fs.unlinkSync(localFilePath)
-    return null;
+      fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+      return null;
   }
-};
+}
+
 
 
 export {uploadOnCloudinary}
