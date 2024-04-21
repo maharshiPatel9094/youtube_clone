2b22572beac6dde7ad54714c590f3c6edf957a4b
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-04-20 20:22:35 -0400

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,6 +219,17 @@ const logoutUser = asyncHandler(async (req,res) => {
 
 })
 
+
+// refresh access token
+const refreshAccessToken = asyncHandler(async(req,res)=> {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, " Unauthorized Request !!! ")
+  }
+
+  jwt
+})
+
 export { 
   registerUser, 
   loginUser,
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,9 +3,9 @@ import fs from "fs";
 import path from "path";
 
 // Ensure environment variables are set
-console.log("CLOUDINARY_CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
-console.log("CLOUDINARY_API_SECRET:", process.env.CLOUDINARY_API_SECRET);
+// console.log("CLOUDINARY_CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
+// console.log("CLOUDINARY_API_SECRET:", process.env.CLOUDINARY_API_SECRET);
 
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
